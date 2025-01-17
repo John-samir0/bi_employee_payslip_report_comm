2c3ec63c510937843a9efb1e5b28f9ca001462e7
John-samir0 <john.botros44@gmail.com>
2025-01-18 01:29:26 +0200

    Add files via upload

---
+++ bi_employee_payslip_report_comm/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from . import wizard
+from . import report
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+
+        # <field name="menu" eval="False"/>
